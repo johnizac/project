7b2b0dc161eab646f186afff91c87b4348370f46
AhShaikh4 <122369871+AhShaikh4@users.noreply.github.com>
2024-04-15 00:26:56 -0400

    Update contactUs.js

--- src/pages/contactUs.js
+++ src/pages/contactUs.js
@@ -1,6 +1,18 @@
+import React from 'react';
 
-export default function ContactUs() {
-    return (
-        <><p>contact Us page</p></>
-    );
-}
+const ContactDetails = () => {
+  return (
+    <div className="contact-details">
+      <h2>Contact Details</h2>
+      <p>Phone number: <a href="tel:77788881111">777 8888 1111</a></p>
+      <p>Email: <a href="mailto:contactus@example.com">Contactus@example.com</a></p>
+      <p>Working Hours:</p>
+      <ul>
+        <li>Mon-Friday 8 AM - 7 PM</li>
+        <li>Sat/Sun - 12pm - 5pm</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContactDetails;
