39c82dd6926906360496ede21b4496b3af97cd06
AhShaikh4 <122369871+AhShaikh4@users.noreply.github.com>
2024-04-15 00:26:18 -0400

    Update aboutUs.js

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,5 +1,20 @@
-export default function AboutUs() {
-    return (
-        <><p>aboutUs</p></>
-    );
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      <div style={{width: '45%', border: '1px solid #ccc', padding: '20px', boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)'}}>
+        <h2>Ahmed Shaikh</h2>
+        <p>Software Engineer</p>
+        
+      </div>
+      <div style={{width: '45%', border: '1px solid #ccc', padding: '20px', boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)'}}>
+        <h2>John Mubeezi</h2>
+        <p>Software Enginner</p>
+       
+      </div>
+    </div>
+  );
 }
+
+export default AboutUs;
